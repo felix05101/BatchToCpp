0be424d8db96bbfff372a83b1ab6dd23935397ab
Felix <87916476+Newcummer@users.noreply.github.com>
2024-03-23 22:19:41 +0100

    improved

--- stupid/stupid.cpp
+++ stupid/stupid.cpp
@@ -3,8 +3,20 @@
 #include <string>
 #include <windows.h>
 
+std::wstring escapeSpecialChars(const std::wstring& line) {
+    std::wstring escapedLine;
+    for (wchar_t c : line) {
+        if (c == L'\\' || c == L'"') {
+            escapedLine += L'\\'; 
+        }
+        escapedLine += c;
+    }
+    return escapedLine;
+}
+
 std::wstring wrapWithSystem(const std::wstring& line) {
-    return L"system(\"" + line + L"\");";
+    std::wstring escapedLine = escapeSpecialChars(line);
+    return L"system(\"" + escapedLine + L"\");";
 }
 
 int main() {
@@ -13,7 +25,7 @@ int main() {
 
     ZeroMemory(&ofn, sizeof(ofn));
     ofn.lStructSize = sizeof(ofn);
-    ofn.lpstrFilter = L"Batch Files (*.bat)\0*.bat\0Newcummer\0*.*\0";
+    ofn.lpstrFilter = L"Batch Files (*.bat)\0*.bat\0All Files (*.*)\0*.*\0";
     ofn.lpstrFile = fileName;
     ofn.nMaxFile = MAX_PATH;
     ofn.Flags = OFN_FILEMUSTEXIST | OFN_PATHMUSTEXIST;
@@ -22,22 +34,31 @@ int main() {
         std::wstring inputFilePath(fileName);
         std::wifstream inputFile(inputFilePath);
         if (inputFile) {
-            std::wstring outputFilePath = inputFilePath + L"_output.txt";
+            std::wstring outputFilePath = inputFilePath + L"_output.cpp";
             std::wofstream outputFile(outputFilePath);
             if (outputFile) {
+                outputFile << "#include <cstdlib>\n\n";
+                outputFile << "int main() {\n";
                 std::wstring line;
                 while (std::getline(inputFile, line)) {
-                    std::wstring wrappedLine = wrapWithSystem(line);
-                    outputFile << wrappedLine << std::endl;
+                    if (!line.empty()) {
+                        std::wstring wrappedLine = wrapWithSystem(line);
+                        outputFile << "    " << wrappedLine << std::endl;
+                    }
                 }
+                outputFile << "    return 0;\n";
+                outputFile << "}\n";
                 outputFile.close();
-
+                std::wcout << L"Conversion completed. Output file: " << outputFilePath << std::endl;
             }
-
+            else {
+                std::cerr << "Error: Unable to open output file." << std::endl;
+            }
         }
-
+        else {
+            std::cerr << "Error: Unable to open input file." << std::endl;
+        }
     }
-
 
     return 0;
 }
